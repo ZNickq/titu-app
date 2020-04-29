532a9c49113347314143438c93a216a2747b0434
Nichita <nichita@adonissoft.com>
2020-04-29 18:37:06 +0300

    Implemented simple transaction logic between good/bad movies and UI update.

--- app/src/main/java/com/titu/tituapp/ui/main/MovieDetailsFragment.kt
+++ app/src/main/java/com/titu/tituapp/ui/main/MovieDetailsFragment.kt
@@ -2,25 +2,109 @@ package com.titu.tituapp.ui.main
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 import com.titu.tituapp.R
+import com.titu.tituapp.databinding.FragmentMovieDetailsBinding
+import com.titu.tituapp.ui.main.vm.MainViewModel
+import kotlinx.android.synthetic.main.fragment_movie_details.*
+import kotlinx.android.synthetic.main.mainview_list_item_movie.view.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class MovieDetailsFragment : Fragment() {
+    private val viewModel: MainViewModel by lazy { MainViewModel() }
+    private lateinit var goodMoviesAdapter: RecyclerViewAdapter
+    private lateinit var badMoviesAdapter: RecyclerViewAdapter
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie_details, container, false)
+        val binding =
+            DataBindingUtil.inflate<FragmentMovieDetailsBinding>(
+                inflater,
+                R.layout.fragment_movie_details,
+                container,
+                false
+            )
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.goodMovies.observe(viewLifecycleOwner, Observer {
+            Log.d("TAG", "badMovies :: $it")
+            if (::goodMoviesAdapter.isInitialized.not()) {
+                goodMoviesAdapter = RecyclerViewAdapter(it, object : MovieClickListener {
+                    override fun onClicked(position: Int) = viewModel.downrateMovie(position)
+                })
+                rvGoodMovies.apply {
+                    layoutManager = LinearLayoutManager(requireContext())
+                    adapter = goodMoviesAdapter
+                }
+            } else {
+                goodMoviesAdapter.notifyDataSetChanged()
+            }
+        })
+        viewModel.badMovies.observe(viewLifecycleOwner, Observer {
+            Log.d("TAG", "goodMovies :: $it")
+            if (::badMoviesAdapter.isInitialized.not()) {
+                badMoviesAdapter = RecyclerViewAdapter(it, object : MovieClickListener {
+                    override fun onClicked(position: Int) = viewModel.uprateMovie(position)
+                })
+                rvBadMovies.apply {
+                    layoutManager = LinearLayoutManager(requireContext())
+                    adapter = badMoviesAdapter
+                }
+            } else {
+                badMoviesAdapter.notifyDataSetChanged()
+            }
+        })
+    }
+}
 
+/**
+ * Adapter for movies Recyclerview
+ */
+class RecyclerViewAdapter(
+    private val movies: List<String>,
+    private val movieClickListener: MovieClickListener
+) :
+    RecyclerView.Adapter<RecyclerViewAdapter.MovieViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        return MovieViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.mainview_list_item_movie,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int = movies.size
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.movieName.text = movies[position]
+        holder.itemView.setOnClickListener { movieClickListener.onClicked(position) }
+    }
+
+    class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val movieName: TextView = itemView.albumNameTextView
+    }
+}
+
+interface MovieClickListener {
+    fun onClicked(position: Int)
 }
--- app/src/main/java/com/titu/tituapp/ui/main/vm/MainViewModel.kt
+++ app/src/main/java/com/titu/tituapp/ui/main/vm/MainViewModel.kt
@@ -1,8 +1,40 @@
 package com.titu.tituapp.ui.main.vm
 
-class MainViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 
-    init {
-        print("New object here!")
+class MainViewModel : ViewModel() {
+    private val _badMovies: MutableList<String> by lazy {
+        mutableListOf(
+            "Titanic",
+            "Liceenii",
+            "Terminator"
+        )
+    }
+    val badMovies: MutableLiveData<List<String>> = MutableLiveData(_badMovies)
+
+    private val _goodMovies: MutableList<String> by lazy {
+        mutableListOf(
+            "Titanic - Part2",
+            "Liceenii - Reluare",
+            "Terminator - But Skynet won"
+        )
+    }
+    val goodMovies: MutableLiveData<List<String>> = MutableLiveData(_goodMovies)
+
+    fun downrateMovie(position: Int) {
+        val movie = _goodMovies.removeAt(position)
+        _badMovies.add(movie)
+
+        badMovies.postValue(_badMovies)
+        goodMovies.postValue(_goodMovies)
+    }
+
+    fun uprateMovie(position: Int) {
+        val movie = _badMovies.removeAt(position)
+        _goodMovies.add(movie)
+
+        badMovies.postValue(_badMovies)
+        goodMovies.postValue(_goodMovies)
     }
 }
